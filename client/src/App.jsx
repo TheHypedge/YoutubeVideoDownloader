39c3a481a773d090fc992b27eec230af7b681ff9
Chiragdbb <schirag414@gmail.com>
2025-03-30 03:44:32 +0530

    Add: test UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,85 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import './index.css'
+import { useState } from "react";
+import "./App.css";
+import "./index.css";
+import axios from "axios"
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [urlValue, setUrlValue] = useState("");
+	const [data, setData] = useState(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='text-8xl'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const handleDownload = async () => {
+		const data = await axios.get(
+			`http://localhost:4000/download?url=${urlValue}`
+		);
+		setData(data);
+		setUrlValue("");
+	};
+
+	return (
+		<div className=" bg- flex flex-col justify-center items-center min-h-screen">
+			<div className="flex flex-row justify-center items-center">
+				<div>
+					{/* <img className=" w-20 h-20" src={logo} alt="logo" /> */}
+				</div>
+				<div className="text-4xl font-bold">
+					<h1>
+						<span className=" text-red-700">Youtube</span>{" "}
+						downloader
+					</h1>
+				</div>
+			</div>
+			<div className="flex flex-row">
+				<div>
+					<input
+						type="text"
+						placeholder="Enter url"
+						value={urlValue}
+						onChange={(e) => setUrlValue(e.target.value)}
+						className=" outline-none p-2 bg-yellow-500 border-2 border-gray-500 rounded-md md:mr-4"
+					/>
+				</div>
+				<div className=" bg-black text-yellow-500 py-2 px-6 rounded-md cursor-pointer">
+					<button onClick={handleDownload}>download</button>
+				</div>
+			</div>
+
+			<div>
+				{data !== null ? (
+					<div>
+						<div className="my-4">
+							<iframe
+								width="570"
+								height="320"
+								src={`${data.data.url}`}
+								title="video"
+							/>
+						</div>
+						<div>
+							{data?.data.info.map((formatName, index) => (
+								<div key={index}>
+									<a
+										href={formatName.url}
+										target="_blank"
+										download
+										className=" outline-none italic underline"
+									>
+										{formatName.mimeType.split(";")[0] +
+											"  "}
+										{formatName.hasVideo
+											? formatName.height + "p"
+											: ""}
+									</a>
+								</div>
+							))}
+						</div>
+					</div>
+				) : (
+					<div className=" text-red-700 font-bold mt-10">
+						No download yet
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
-
-export default App
+export default App;
